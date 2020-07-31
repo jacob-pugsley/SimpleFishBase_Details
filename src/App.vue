786d8cb5ca378bb74f76f580bdf339005de3539e
Jacob Pugsley <jacob.pugsley@wsu.edu>
2020-07-31 13:36:09 -0700

    added files from old project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		
+			<div id="detDiv" is="detailsComponent" v-for="result in results" :key="result.taxonomy" :fish="result"></div> 
+		
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import detailsComponent from '@/views/details.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'details',
+	components: {
+		detailsComponent
+	},
+	data(){
+		return{
+			taxonomy: '',
+			results: []
+		}
+	},
+	watch: {
+		'$route.params.taxonomy': function(){
+			//alert(this.$route.params.taxonomy)
+			this.search(this.$route.params.taxonomy)
+		}
+	},
+	methods:{
+		search: function(taxon){
+			this.results = []
+			let res = this.results
+			//let pt = []
+			let config = {
+				headers: {'Access-Control-Allow-Origin': '*'}
+			}
+			let url = "http://localhost:5000/params="
+			url = url + "$" + taxon + "$"	
+			//alert(url)
+
+			axios.get(url, config)
+			//after successful call
+			.then( function (response) {
+				res.push(response.data["results"][0])
+			})
+			//after error
+			.catch( function (error) {
+				alert('details search: ' + error)
+			})
+		}
+	}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#detDiv{
+	width: 70%;
+	margin: auto;
+	margin-top: 5%;
+
 }
 </style>
